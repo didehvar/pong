4adc59881a4a24310527fd98ff0b70fc70440332
james <james@didehvar.com>
2015-07-24 23:43:15 +0100

    Just gulping around

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,57 @@
 var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
 
-gulp.task('default', function() {
-
+gulp.task('connect', function() {
+  plugins.connect.server({
+    root: 'build',
+    livereload: true,
+    port: 8888
+  });
 });
+
+gulp.task('html', function() {
+  return gulp.src('app/index.html')
+    .pipe(gulp.dest('build'))
+    .pipe(plugins.connect.reload());
+});
+
+gulp.task('js', function() {
+  return gulp.src('app/js/*.js')
+    .pipe(plugins.traceur())
+    .pipe(plugins.rename({ extname: '.min.js' }))
+    .pipe(gulp.dest('build/js'))
+    .pipe(plugins.connect.reload())
+    .on('error', plugins.util.log);
+});
+
+gulp.task('vendor', function() {
+  return gulp.src('app/js/vendor/*.js')
+    .pipe(plugins.concat('vendor.js'))
+    .pipe(plugins.uglify())
+    .pipe(plugins.rename({ extname: '.min.js' }))
+    .pipe(gulp.dest('build/js'))
+    .pipe(plugins.connect.reload())
+    .on('error', plugins.util.log);
+});
+
+gulp.task('img', function() {
+  return gulp.src('app/img/*.*')
+    .pipe(gulp.dest('build/img'))
+    .pipe(plugins.connect.reload());
+});
+
+gulp.task('clean', function() {
+  return gulp.src('build', { read: false })
+    .pipe(plugins.clean());
+});
+
+gulp.task('watch', function() {
+  gulp.watch('app/index.html', ['html']);
+  gulp.watch('app/js/*.js', ['js']);
+  gulp.watch('app/js/vendor/*.js', ['vendor']);
+  gulp.watch('app/img/*.*', ['img']);
+});
+
+gulp.task('build', ['html', 'js', 'vendor', 'img']);
+
+gulp.task('default', ['connect', 'watch']);
