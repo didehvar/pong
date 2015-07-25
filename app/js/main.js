0d4cfaf6614d4a3e023e600ef91fa27c5adda71a
james <james@didehvar.com>
2015-07-25 15:08:52 +0100

    Maybe this should go in preload?

--- app/js/main.js
+++ app/js/main.js
@@ -4,17 +4,17 @@ window.onload = function() {
 
   var leftPaddle = new Paddle(game);
   var rightPaddle = new Paddle(game);
+  var bitmap;
 
   function preload () {
-  }
-
-  function create () {
-    var bitmap = this.game.add.bitmapData(4, 40);
+    bitmap = this.game.add.bitmapData(4, 40);
     bitmap.ctx.beginPath();
     bitmap.ctx.rect(0, 0, bitmap.width, bitmap.height);
     bitmap.ctx.fillStyle = '#fff';
     bitmap.ctx.fill();
+  }
 
+  function create () {
     leftPaddle.create(50, this.game.world.centerY, bitmap);
     rightPaddle.create(this.game.world.width - 50, this.game.world.centerY, bitmap);
   }
