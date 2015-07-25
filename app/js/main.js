cfb18ea0d107afa78b39dc055f3db58fa75896dc
james <james@didehvar.com>
2015-07-25 14:46:47 +0100

    Paddles

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,22 @@
 
 window.onload = function() {
+  var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
 
-    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create });
-    var paddle = new Paddle(game);
+  var leftPaddle = new Paddle(game);
+  var rightPaddle = new Paddle(game);
 
-    function preload () {
+  function preload () {
+  }
 
-        paddle.preload('paddle');
+  function create () {
+    var bitmap = this.game.add.bitmapData(4, 40);
+    bitmap.ctx.beginPath();
+    bitmap.ctx.rect(0, 0, bitmap.width, bitmap.height);
+    bitmap.ctx.fillStyle = '#fff';
+    bitmap.ctx.fill();
 
-    }
-
-    function create () {
-
-        var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
-
-    }
+    leftPaddle.create(50, this.game.world.centerY, bitmap);
+    rightPaddle.create(this.game.world.width - 50, this.game.world.centerY, bitmap);
+  }
 
 };
--- app/js/paddle.js
+++ app/js/paddle.js
@@ -4,14 +4,8 @@ class Paddle {
     this.game = game;
   }
 
-  preload(key) {
-    var bitmap = this.game.add.bitmapData(5, 40);
-    bitmap.ctx.beginPath();
-    bitmap.ctx.rect(0, 0, bitmap.width, bitmap.height);
-    bitmap.ctx.fillStyle = '#fff';
-    bitmap.ctx.fill();
-
-    this.sprite = this.game.add.sprite(30, this.game.world.centerY, bitmap);
+  create(x, y, bitmap) {
+    this.sprite = this.game.add.sprite(x, y, bitmap);
     this.sprite.anchor.set(0.5, 0.5);
   }
 }
