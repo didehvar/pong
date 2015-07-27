ef0a5ffe178303a79ac405d2f30e4e47014425a8
james <james@didehvar.com>
2015-07-27 15:29:57 +0100

    Give the ball a more random direction

--- app/js/ball.js
+++ app/js/ball.js
@@ -25,7 +25,7 @@ class Ball extends Sprite {
 
     var timer = this.game.time.events.add(500, function() {
       this.sprite.body.velocity.x = directionX > 0.5 ? Ball.SPEED : -Ball.SPEED;
-      this.sprite.body.velocity.y = Math.random() * 100;
+      this.sprite.body.velocity.y = Math.random() * 400;
     }, this);
   }
 
